517e1319c1192059196440ec847a7227ab847749
HolyMotherOfFarming <holymotheroffarming@gmail.com>
2016-11-17 18:13:19 -0500

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,6 @@
+alert('Turn volume off/mute this tab if you do not want to hear the best music ever made, and hit "ok."');
+alert('Please take a few moments to donate to our fine cause.')
+prompt('How much will you donate?')
+alert('not enough')
+prompt('How much will you donate?')
+alert('ok')
